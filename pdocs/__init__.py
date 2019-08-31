f69884dbd362a98f69bb503a31332d5b4b15ca1e
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-31 00:26:11 -0700

    Bump version

--- pdocs/__init__.py
+++ pdocs/__init__.py
@@ -172,7 +172,7 @@ If `__pdoc__[key] = None`, then `key` will not be included in the
 public interface of the module.
 """
 
-__version__ = "0.1.1"
+from pdoc.version import VERSION
 """
 The current version of pdoc. This value is read from `setup.py`.
 """
--- pdocs/version.py
+++ pdocs/version.py
@@ -1,1 +1,1 @@
-VERSION = "0.4"
+VERSION = "0.1.2"
